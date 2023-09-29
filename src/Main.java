ad3058d1879605a1cbe488dc6ff7757fc640d100
MAxlekansder <144068201+MAxlekansder@users.noreply.github.com>
2023-09-29 20:08:40 +0200

    uppgift 6-1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] forArray = {1,2,3,50,40,100,200};
 
+        for (int i = 0; i < forArray.length; i++) {
+            System.out.println(forArray[i]);
+        }
     }
 }
